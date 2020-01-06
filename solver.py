734197f84e600c9814c05b8c829338e8bf5c9f7c
joji <jojitenges@hotmail.com>
2020-01-06 17:30:39 +1100

    attempt at solver using blocks

--- solver.py
+++ solver.py
@@ -6,17 +6,6 @@ def makeGrid(numOfRows: int, numOfColumns: int) -> List[List[bool]]:
         grid.append([False] * numOfColumns)
     return grid
 
-def getRowBlocks(row: List[bool]) -> List[int]:
-    rowBlocks: List[int] = []
-    length: int = 0
-    for i in row:
-        if i:
-            length += 1
-        elif length != 0:
-            rowBlocks.append(length)
-            length = 0
-    return rowBlocks
-
 def getColumnBlocks(grid: List[List[bool]], index: int) -> List[int]:
     columnBlocks: List[int] = []
     length: int = 0
@@ -30,21 +19,8 @@ def getColumnBlocks(grid: List[List[bool]], index: int) -> List[int]:
             length = 0
     return columnBlocks
 
-def isSolvable(grid: List[List[bool]], rows: List[List[int]], columns: List[List[int]]) -> bool:
-    numOfRows: int = len(rows)
+def isSolvable(grid: List[List[bool]], columns: List[List[int]]) -> bool:
     numOfColumns: int = len(columns)
-
-    # Check each row in the grid
-    rowBlock: List[int] = []
-    rowCondition: List[int] = []
-    for i in range(numOfRows):
-        rowBlock = getRowBlocks(grid[i])
-        rowCondition = rows[i]
-        if len(rowBlock) > len(rowCondition):
-            return False
-        for j in range(len(rowBlock)):
-            if rowBlock[j] > rowCondition[j]:
-                return False
     
     # Check each column in the grid
     columnBlock: List[int] = []
@@ -60,14 +36,69 @@ def isSolvable(grid: List[List[bool]], rows: List[List[int]], columns: List[List
     
     return True
 
+def isSolved(grid: List[List[bool]], columns: List[List[int]]) -> bool:
+    numOfColumns: int = len(columns)
+    
+    # Check each column in the grid
+    columnBlock: List[int] = []
+    columnCondition: List[int] = []
+    for i in range(numOfColumns):
+        columnBlock = getColumnBlocks(grid, i)
+        columnCondition = columns[i]
+        if len(columnBlock) != len(columnCondition):
+            return False
+        for j in range(len(columnBlock)):
+            if columnBlock[j] != columnCondition[j]:
+                return False
+    
+    return True
 
-def solveGrid(rows: List[List[int]], columns: List[List[int]]) -> List[List[bool]]:
-    grid: List[List[bool]] = makeGrid(len(rows), len(columns))
-    grid[0][0] = True
-    grid[1][0] = True
-    grid[2][0] = True
-    print(isSolvable(grid, rows, columns))
-    return grid
+def getRowStartingIndex(row: List[bool]):
+    for i in range(0, len(row)):
+        if row[i]:
+            if i + 1 >= len(row):
+                return -1
+            else:
+                return i + 1
+    return 0
+
+def getRemainingBlockLength(rowBlocks: List[int]):
+    length: int = 0
+    for i in rowBlocks:
+        length += i + 1
+    return length
+
+def solveGrid(
+    rowsToAdd: List[List[int]],
+    numberOfRows: int,
+    columnConditions: List[List[int]],
+    currentGrid: List[List[bool]]=None) -> List[List[bool]]:
+    # Make a new grid at the start of call chain
+    if currentGrid == None:
+        currentGrid = makeGrid(len(rowsToAdd), len(columnConditions))
+
+    if isSolved(currentGrid, columnConditions):
+        print(currentGrid)
+        return currentGrid
+    
+    blockLength: int = rowsToAdd[0].pop(0)
+    if len(rowsToAdd[0]) == 0:
+        rowsToAdd.pop(0)
+    if len(rowsToAdd) == 0:
+        return
+    otherBlocksLength: int = getRemainingBlockLength(rowsToAdd[0])
+    currentRowIndex: int = numberOfRows - len(rowsToAdd)
+    firstFreeIndex: int = getRowStartingIndex(currentGrid[currentRowIndex])
+
+    gridCopy: List[List[bool]]
+    for i in range(firstFreeIndex, len(currentGrid[currentRowIndex]) - otherBlocksLength - blockLength):
+        gridCopy = [row[:] for row in currentGrid]
+        for j in range(0, blockLength):
+            gridCopy[currentRowIndex][i + j] = True
+        if isSolvable(gridCopy, columnConditions):
+            solveGrid(rowsToAdd, numberOfRows, columnConditions, currentGrid=gridCopy)
+
+    return [1]
 
 if __name__ == '__main__':
     # input for solver
@@ -101,4 +132,4 @@ if __name__ == '__main__':
         [1],
         [1],
     ]
-    print(solveGrid(rows, columns))
+    print(solveGrid(rows, len(rows), columns))
