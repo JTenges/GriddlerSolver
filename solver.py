98376497f4a6ac80356c03642add6e94e84a01d8
joji <jojitenges@hotmail.com>
2020-01-17 21:54:01 +1100

    finished solver. Very slow though

--- solver.py
+++ solver.py
@@ -1,135 +1,245 @@
 from typing import List
 
-def makeGrid(numOfRows: int, numOfColumns: int) -> List[List[bool]]:
-    grid: List[List[bool]] = []
-    for _ in range(0, numOfRows):
-        grid.append([False] * numOfColumns)
-    return grid
+class MyGrid():
 
-def getColumnBlocks(grid: List[List[bool]], index: int) -> List[int]:
-    columnBlocks: List[int] = []
-    length: int = 0
-    i: bool
-    for row in grid:
-        i = row[index]
-        if i:
-            length += 1
-        elif length != 0:
+    def __init__(self, remainingRows: List[List[int]], columnConditions: List[List[int]], grid: List[List[bool]]=None):
+        # Initialise a new grid if one is not given
+        if grid == None:
+            self.grid = []
+            for _ in range(0, len(remainingRows)):
+                self.grid.append([False] * len(columnConditions))
+        else:
+            self.grid = grid
+
+        self.remainingRows = remainingRows
+        self.currentRowIndex = 0
+        self.columnConditions = columnConditions
+    
+    def getColumnBlocks(self, index: int) -> List[int]:
+        columnBlocks: List[int] = []
+        length: int = 0
+        i: bool
+        for row in self.grid:
+            i = row[index]
+            if i:
+                length += 1
+            elif length != 0:
+                columnBlocks.append(length)
+                length = 0
+        # Add final columnBlock
+        if length != 0:
             columnBlocks.append(length)
-            length = 0
-    return columnBlocks
+        return columnBlocks
 
-def isSolvable(grid: List[List[bool]], columns: List[List[int]]) -> bool:
-    numOfColumns: int = len(columns)
-    
-    # Check each column in the grid
-    columnBlock: List[int] = []
-    columnCondition: List[int] = []
-    for i in range(numOfColumns):
-        columnBlock = getColumnBlocks(grid, i)
-        columnCondition = columns[i]
-        if len(columnBlock) > len(columnCondition):
-            return False
-        for j in range(len(columnBlock)):
-            if columnBlock[j] > columnCondition[j]:
+    def columnsSolved(self) -> bool:
+        # Check each column in the grid
+        columnBlock: List[int] = []
+        columnCondition: int = []
+        for i in range(len(self.columnConditions)):
+            columnBlock = self.getColumnBlocks(i)
+            columnCondition = self.columnConditions[i]
+            if len(columnBlock) != len(columnCondition):
                 return False
+            for j in range(len(columnBlock)):
+                if columnBlock[j] != columnCondition[j]:
+                    return False
+        
+        return True
     
-    return True
+    def placeNextBlock(self, positionInRow: int):
+        blockLength = self.remainingRows[0].pop(0)
+        currentRow = self.grid[self.currentRowIndex]
+        if self.remainingRows[0] == []:
+            self.currentRowIndex += 1
+            self.remainingRows.pop(0)
+        for i in range(positionInRow, positionInRow + blockLength):
+            currentRow[i] = True
 
-def isSolved(grid: List[List[bool]], columns: List[List[int]]) -> bool:
-    numOfColumns: int = len(columns)
-    
-    # Check each column in the grid
-    columnBlock: List[int] = []
-    columnCondition: List[int] = []
-    for i in range(numOfColumns):
-        columnBlock = getColumnBlocks(grid, i)
-        columnCondition = columns[i]
-        if len(columnBlock) != len(columnCondition):
-            return False
-        for j in range(len(columnBlock)):
-            if columnBlock[j] != columnCondition[j]:
+    def isSafePlacement(self, positionInRow: int):
+        myGridCopy = self.copy()
+        myGridCopy.placeNextBlock(positionInRow)
+
+        # Check if placement overfills collumn
+        columnBlock: List[int] = []
+        columnCondition: int = []
+        for i in range(len(myGridCopy.columnConditions)):
+            columnBlock = myGridCopy.getColumnBlocks(i)
+            columnCondition = myGridCopy.columnConditions[i]
+            if len(columnBlock) > len(columnCondition):
                 return False
+            for j in range(len(columnBlock)):
+                if columnBlock[j] > columnCondition[j]:
+                    return False
+        
+        return True
+
     
-    return True
+    def validRowIndexes(self) -> range:
+        currentRow = self.grid[self.currentRowIndex]
+        startingIndex: int = -1
+        for i in range(len(currentRow), -1, -1):
+            if i == 0:
+                startingIndex = 0
+            if currentRow[i - 1]:
+                startingIndex = i + 1
+                break
+        
+        endIndex: int = -1
+        rowsLeftover = self.remainingRows[0][1:]
+        endIndex = len(self.grid[0]) - sum(rowsLeftover) - len(rowsLeftover) - self.remainingRows[0][0] + 1
+        return range(startingIndex, endIndex)
 
-def getRowStartingIndex(row: List[bool]):
-    for i in range(0, len(row)):
-        if row[i]:
-            if i + 1 >= len(row):
-                return -1
-            else:
-                return i + 1
-    return 0
+    def copy(self):
+        gridCopy = []
+        for row in self.grid:
+            gridCopy.append(row[:])
+        
+        remainingRowsCopy = []
+        for condition in self.remainingRows:
+            remainingRowsCopy.append(condition[:])
+        
+        columnConditionCopy = []
+        for condition in self.columnConditions:
+            columnConditionCopy.append(condition[:])
 
-def getRemainingBlockLength(rowBlocks: List[int]):
-    length: int = 0
-    for i in rowBlocks:
-        length += i + 1
-    return length
+        myGridCopy = MyGrid(remainingRowsCopy, columnConditionCopy, gridCopy)
+        myGridCopy.currentRowIndex = self.currentRowIndex
+        return myGridCopy
 
-def solveGrid(
-    rowsToAdd: List[List[int]],
-    numberOfRows: int,
-    columnConditions: List[List[int]],
-    currentGrid: List[List[bool]]=None) -> List[List[bool]]:
-    # Make a new grid at the start of call chain
-    if currentGrid == None:
-        currentGrid = makeGrid(len(rowsToAdd), len(columnConditions))
-
-    if isSolved(currentGrid, columnConditions):
-        print(currentGrid)
-        return currentGrid
+def isSolvable(myGrid: MyGrid) -> bool:
+    if len(myGrid.remainingRows) <= 0:
+        if myGrid.columnsSolved():
+            print(myGrid.grid)
+            for row in myGrid.grid:
+                for block in row:
+                    if block:
+                        print('██', end='')
+                    else:
+                        print('░░', end='')
+                print()
+            return True
+        else:
+            return False
     
-    blockLength: int = rowsToAdd[0].pop(0)
-    if len(rowsToAdd[0]) == 0:
-        rowsToAdd.pop(0)
-    if len(rowsToAdd) == 0:
-        return
-    otherBlocksLength: int = getRemainingBlockLength(rowsToAdd[0])
-    currentRowIndex: int = numberOfRows - len(rowsToAdd)
-    firstFreeIndex: int = getRowStartingIndex(currentGrid[currentRowIndex])
+    
+    validIndexes: range = myGrid.validRowIndexes()
+    for index in validIndexes:
+        gridCopy: MyGrid = myGrid.copy()
+        if gridCopy.isSafePlacement(index):
+            gridCopy.placeNextBlock(index)
+            if isSolvable(gridCopy):
+                return True
+        
+    return False
 
-    gridCopy: List[List[bool]]
-    for i in range(firstFreeIndex, len(currentGrid[currentRowIndex]) - otherBlocksLength - blockLength):
-        gridCopy = [row[:] for row in currentGrid]
-        for j in range(0, blockLength):
-            gridCopy[currentRowIndex][i + j] = True
-        if isSolvable(gridCopy, columnConditions):
-            solveGrid(rowsToAdd, numberOfRows, columnConditions, currentGrid=gridCopy)
 
-    return [1]
+
 
 if __name__ == '__main__':
-    # input for solver
+    # columns = [
+    #     [1],
+    #     [1],
+    #     [1]
+    # ]
+
+    # rows = [
+    #     [1, 1],
+    #     [1]
+    # ]
+    # myGrid = MyGrid(rows, columns)
+    # isSolvable(myGrid)
+
+    # columns = [
+    #     [2],
+    #     [2, 3],
+    #     [1, 3, 2],
+    #     [2, 2, 1],
+    #     [1, 5, 1],
+    #     [1, 1, 11],
+    #     [1, 5, 1],
+    #     [5, 2],
+    #     [1, 3, 3],
+    #     [2, 3],
+    # ]
+
+    # rows = [
+    #     [1],
+    #     [1, 1, 1, 1],
+    #     [1, 1, 1, 1, 1],
+    #     [3, 3],
+    #     [1, 5],
+    #     [7],
+    #     [5],
+    #     [3],
+    #     [2, 3],
+    #     [3, 1, 1],
+    #     [3, 1, 2],
+    #     [2, 3],
+    #     [4],
+    #     [1],
+    #     [1],
+    # ]
+
     columns = [
-        [2],
-        [2, 3],
-        [1, 3, 2],
-        [2, 2, 1],
-        [1, 5, 1],
-        [1, 1, 11],
-        [1, 5, 1],
-        [5, 2],
-        [1, 3, 3],
-        [2, 3],
+        [8],
+        [9],
+        [7, 3],
+        [8, 2],
+        [8, 2],
+        [10, 2, 1, 1],
+        [9, 5, 2],
+        [9, 10],
+        [9, 6, 2],
+        [9, 11],
+        [6, 2, 4, 7, 2],
+        [9, 1, 3, 7, 2],
+        [13, 9, 1],
+        [5, 17, 1],
+        [1, 4, 14, 1],
+        [2, 4, 12, 1],
+        [1, 4, 8],
+        [1, 5, 4],
+        [1, 5],
+        [1, 5],
+        [1, 4],
+        [1, 4],
+        [1, 3],
+        [1, 2],
+        [1, 1],
     ]
 
     rows = [
-        [1],
-        [1, 1, 1, 1],
-        [1, 1, 1, 1, 1],
-        [3, 3],
-        [1, 5],
-        [7],
         [5],
-        [3],
-        [2, 3],
-        [3, 1, 1],
-        [3, 1, 2],
-        [2, 3],
+        [4, 10],
+        [5],
+        [15],
+        [14],
+        [3, 9],
+        [4, 7],
+        [3, 3],
         [4],
-        [1],
-        [1],
+        [4],
+        [4, 4],
+        [12],
+        [5, 5],
+        [8, 4],
+        [9, 5],
+        [16],
+        [9, 6],
+        [8, 7],
+        [8, 7],
+        [6, 7],
+        [6, 9],
+        [4, 9],
+        [2, 10],
+        [13],
+        [4, 5],
+        [3, 5],
+        [2, 1, 1],
+        [1, 1, 1],
+        [6],
+        [11],
     ]
-    print(solveGrid(rows, len(rows), columns))
+    myGrid = MyGrid(rows, columns)
+    isSolvable(myGrid)
+    
---
+++ solver_test.py
@@ -0,0 +1,223 @@
+import unittest
+from solver import MyGrid
+class TestMyGrid(unittest.TestCase):
+
+    def test_getColumnBlocks(self):
+        columns = [
+            [1],
+            [1]
+        ]
+
+        rows = [
+            [1],
+            [1]
+        ]
+        
+        myGrid = MyGrid(rows, columns)
+        self.assertEqual(myGrid.getColumnBlocks(0), [])
+
+        grid = [
+            [True, False],
+            [False, True]
+        ]
+        myGrid = MyGrid(rows, columns, grid=grid)
+        self.assertEqual(myGrid.getColumnBlocks(0), [1])
+
+        columns = [
+            [1]
+        ]
+
+        rows = [
+            [1]
+        ]
+
+        grid = [
+            [True]
+        ]
+        myGrid = MyGrid(rows, columns, grid=grid)
+        self.assertEqual(myGrid.getColumnBlocks(0), [1])
+
+    def test_columnsSolved(self):
+        columns = [
+            [1],
+            [1]
+        ]
+
+        rows = [
+            [1],
+            [1]
+        ]
+        
+        myGrid = MyGrid(rows, columns)
+        self.assertEqual(myGrid.columnsSolved(), False)
+
+        grid = [
+            [True, False],
+            [False, True]
+        ]
+        myGrid = MyGrid(rows, columns, grid=grid)
+        self.assertEqual(myGrid.columnsSolved(), True)
+
+        columns = [
+            [1]
+        ]
+
+        rows = [
+            [1]
+        ]
+
+        grid = [
+            [True]
+        ]
+        myGrid = MyGrid(rows, columns, grid=grid)
+        self.assertEqual(myGrid.columnsSolved(), True)
+    
+    def test_placeNextBlock(self):
+        columns = [
+            [1]
+        ]
+
+        rows = [
+            [1]
+        ]
+        myGrid = MyGrid(rows, columns)
+        self.assertEqual(myGrid.grid, [[False]])
+        myGrid.placeNextBlock(0)
+        self.assertEqual(myGrid.grid, [[True]])
+        self.assertEqual(myGrid.remainingRows, [])
+
+        columns = [
+            [1],
+            [1]
+        ]
+        rows = [
+            [1],
+            [1]
+        ]
+        myGrid = MyGrid(rows, columns)
+        self.assertEqual(myGrid.grid, [[False, False], [False, False]])
+        self.assertEqual(myGrid.remainingRows, [[1], [1]])
+        myGrid.placeNextBlock(0)
+        self.assertEqual(myGrid.grid, [[True, False], [False, False]])
+        self.assertEqual(myGrid.remainingRows, [[1]])
+        myGrid.placeNextBlock(0)
+        self.assertEqual(myGrid.grid, [[True, False], [True, False]])
+        self.assertEqual(myGrid.remainingRows, [])
+    
+    def test_validRowIndexes(self):
+        columns = [
+            [1],
+            [1]
+        ]
+        rows = [
+            [1],
+            [1]
+        ]
+        myGrid = MyGrid(rows, columns)
+        self.assertEqual(myGrid.validRowIndexes(), range(0, 2))
+
+        columns = [
+            [1],
+            [1],
+            [1]
+        ]
+        rows = [
+            [1, 1],
+            [1],
+            [1]
+        ]
+        myGrid = MyGrid(rows, columns)
+        self.assertEqual(myGrid.validRowIndexes(), range(0, 1))
+
+        columns = [
+            [],
+            [],
+            [],
+            [],
+            []
+        ]
+        rows = [
+            [1, 1, 1]
+        ]
+        myGrid = MyGrid(rows, columns)
+        self.assertEqual(myGrid.validRowIndexes(), range(0, 1))
+
+        columns = [
+            [],
+            [],
+            [],
+            [],
+            []
+        ]
+        rows = [
+            [1, 1]
+        ]
+        grid = [[True, False, False, False, False]]
+        myGrid = MyGrid(rows, columns, grid=grid)
+        self.assertEqual(myGrid.validRowIndexes(), range(2, 3))
+
+        columns = [
+            [],
+            [],
+            [],
+            [],
+            [],
+            []
+        ]
+        rows = [
+            [1, 2]
+        ]
+        grid = [[True, False, False, False, False, False]]
+        myGrid = MyGrid(rows, columns, grid=grid)
+        self.assertEqual(myGrid.validRowIndexes(), range(2, 3))
+
+        
+    
+    def test_isSafePlacement(self):
+        columns = [
+            [1],
+            [1]
+        ]
+        rows = [
+            [1],
+            [1]
+        ]
+        myGrid = MyGrid(rows, columns)
+        self.assertEqual(myGrid.isSafePlacement(0), True)
+
+        columns = [
+            [1],
+            [1]
+        ]
+        rows = [
+            [1]
+        ]
+        grid = [
+            [False, False],
+            [True, False]
+        ]
+        myGrid = MyGrid(rows, columns, grid=grid)
+        self.assertEqual(myGrid.isSafePlacement(0), False)
+    
+    def test_copy(self):
+        columns = [
+            [1],
+            [1]
+        ]
+        rows = [
+            [1],
+            [1]
+        ]
+        myGrid1 = MyGrid(rows, columns)
+        myGrid2 = myGrid1.copy()
+        self.assertEqual(myGrid1.remainingRows == myGrid2.remainingRows, True)
+        self.assertEqual(myGrid1.remainingRows is myGrid2.remainingRows, False)
+        self.assertEqual(myGrid1.columnConditions == myGrid2.columnConditions, True)
+        self.assertEqual(myGrid1.columnConditions is myGrid2.columnConditions, False)
+        self.assertEqual(myGrid1.grid == myGrid2.grid, True)
+        self.assertEqual(myGrid1.grid is myGrid2.grid, False)
+        self.assertEqual(myGrid1.currentRowIndex == myGrid2.currentRowIndex, True)
+    
+
+if __name__ == '__main__':
+    unittest.main()
